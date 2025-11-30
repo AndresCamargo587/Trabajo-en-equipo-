7158a6b01848efaac2a8b74bea8021e3ac824d18
AndresCamargo <adcamargon@udistrital.edu.co>
2025-11-29 22:24:21 -0500

    Inicio del menú

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Menu");
+        System.out.println("1. Triqui");
+        System.out.println("2. Calculadora");
+        System.out.println("3. Parejas locas");
+        System.out.println("4. Ahorcado");
+        System.out.print("Seleccione una opción: ");
+
+        int opcion = leer.nextInt();
+
+        switch (opcion) {
+            case 1:
+                Triqui.();
+                break;
+            case 2:
+                Calculadora.();
+                break;
+            case 3:
+                Parejaslocas.();
+                break;
+            case 4:
+                Ahorcado.();
+        }
+    }
 }
+
+
