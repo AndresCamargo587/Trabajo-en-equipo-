b0a6f98c8d201314cfab9b998c214f13355f1a72
nicolas.garridorincon <ngarridor@udistrital.edu.co>
2025-12-03 09:59:43 -0500

    La clase DesarolloParejasLocas es creada y con ello se desarolla la parte de la creación de la ventana del juego también se crean los cuadritos en los que van a ir las letras.

--- src/Parejaslocas.java
+++ src/Parejaslocas.java
@@ -1,3 +1,13 @@
-public class Parejaslocas {
+import javax.swing.*;
 
+public class Parejaslocas {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            DesarrolloParejasLocas juego = new DesarrolloParejasLocas();
+            juego.setVisible(true);
+
+            InstruccionesParejasLocas instrucciones = new InstruccionesParejasLocas();
+            instrucciones.setVisible(true);
+        });
+    }
 }
